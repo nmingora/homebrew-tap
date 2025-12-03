cab78a8383219f2961f3c0463a31df0b3be944e3
Nicolas Mingorance-Geraldo <nicolas.mingorance14@gmail.com>
2025-12-02 19:58:53 -0500

    Rename formula to coderguy

--- coderguy.rb
+++ coderguy.rb
@@ -1,10 +1,10 @@
-class Bridge < Formula
+class Coderguy < Formula
   include Language::Python::Virtualenv
 
   desc "Switchboard for Local vs Cloud AI Coding"
   homepage "https://github.com/nmingora/bridge-cli"
-  url "https://github.com/nmingora/bridge-cli/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "63ccfc3841242b4ce96046e88e6be64f682fc53e1be9419cc2080d19b463a912"
+  url "https://github.com/nmingora/bridge-cli/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "e7a045865ec74460517b4cdf4549ee7233dbf6411901c65bf746c81433b07e50"
   license "MIT"
 
   depends_on "python@3.12"
@@ -14,6 +14,6 @@ class Bridge < Formula
   end
 
   test do
-    system "#{bin}/bridge", "--help"
+    system "#{bin}/coderguy", "--help"
   end
 end
