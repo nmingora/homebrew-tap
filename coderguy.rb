fe6d7206a314bd4a2f6dd7c0588354d66f6a0593
Nicolas Mingorance-Geraldo <nicolas.mingorance14@gmail.com>
2025-12-03 20:43:08 -0500

    Update to v1.0.3

--- coderguy.rb
+++ coderguy.rb
@@ -3,11 +3,12 @@ class Coderguy < Formula
 
   desc "Switchboard for Local vs Cloud AI Coding"
   homepage "https://github.com/nmingora/bridge-cli"
-  url "https://github.com/nmingora/bridge-cli/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "e7a045865ec74460517b4cdf4549ee7233dbf6411901c65bf746c81433b07e50"
+  url "https://github.com/nmingora/bridge-cli/archive/refs/tags/v1.0.2.tar.gz" # Update version!
+  sha256 "8803f76a3a3f5adb42721c71e292b99adf52e4f0b3a3a5604869c14b1f9badad"
   license "MIT"
 
   depends_on "python@3.12"
+  depends_on "ollama"  # <--- THIS IS NEW
 
   def install
     virtualenv_install_with_resources
